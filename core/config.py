48eac144cb54fd9ad9d3727fea642af3f1767a24
mohammad-amarlo <Mhghy92@gmail.com>
2025-11-24 16:40:04 +0330

    Create config.py

---
+++ core/config.py
@@ -0,0 +1,13 @@
+from cryptography.fernet import Fernet
+
+# RubikaDastyarbot 🎖 Royal NextGen Config v0.3.1
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+OWNER_ID = "YOUR_GUID"
+CHANNEL_GUID = "YOUR_CHANNEL_GUID"
+
+CREATOR_NAME_FA = "محمد عمارلو"
+CREATOR_NAME_EN = "Mohammad_Amarloo"
+PROJECT_VERSION = "v0.3.1"
+ROYAL_SIGNATURE = "Origin_Layer_Zero / Royal_NextGen"
+
+FERNET_KEY = "GENERATE_WITH_Fernet.generate_key()"
